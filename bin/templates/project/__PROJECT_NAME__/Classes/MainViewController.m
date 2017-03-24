ea99007cb181b1b0078e6ddb730ba8bf5cd4617a
Alex-Yves CREPIN <a.crepin@xtraball.com>
2017-03-24 17:41:27 +0100

    feat: main view controller

--- bin/templates/project/__PROJECT_NAME__/Classes/MainViewController.m
+++ bin/templates/project/__PROJECT_NAME__/Classes/MainViewController.m
@@ -26,8 +26,6 @@
 //
 
 #import "MainViewController.h"
-//import remoteControls AppsMobileCompany
-//#import "RemoteControls.h"
 
 @implementation MainViewController
 
@@ -76,26 +74,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // AppsMobileCompany
-    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
-    // Do any additional setup after loading the view from its nib.
-    // AppsMobileCompany
-    [[RemoteControls remoteControls] setWebView:self.webView];
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-    // Turn off remote control event delivery AppsMobileCompany
-    [[UIApplication sharedApplication] endReceivingRemoteControlEvents];
-}
-
-
-// AppsMobileCompany
-- (void)remoteControlReceivedWithEvent:(UIEvent *)receivedEvent {
-    [[RemoteControls remoteControls] receiveRemoteEvent:receivedEvent];
 }
 
 /* Comment out the block below to over-ride */
